e3276c3ca8d8a7eb635ede37b602cc8cec291e0c
fly1tkg <fly1tkg@gmail.com>
2014-09-21 06:31:18 +0900

    implement countdown function

--- javascripts/timer.js
+++ javascripts/timer.js
@@ -3,6 +3,7 @@ function Timer(canvas) {
   ctx.scale(-1, 1);
 
   var goal;
+  var timerId;
 
   function clear() {
     ctx.clearRect(0, 0, - canvas.width, canvas.height);
@@ -13,18 +14,26 @@ function Timer(canvas) {
     ctx.fillText(text, - canvas.width / 2, canvas.height / 2);
   }
 
+  function getImage() {
+    clear();
+
+    var now = moment();
+
+    if (goal.unix() - now.unix() > 0) {
+      var rest = moment(goal).subtract(now);
+      draw(rest.format('mm:ss'));
+    } else {
+      clearInterval(timerId);
+      draw('00:00');
+    }
+
+    return canvas.toDataURL();
+  }
+
   return {
-    start: function(sec) {
+    start: function(sec, callback) {
              goal = moment().add(sec, 'second');
-           },
-    getImage: function() {
-                clear();
-
-                var now = moment();
-                var rest = moment(goal).subtract(now);
-
-                draw(rest.format('mm:ss'));
-                return canvas.toDataURL();
-              }
+             timerId = setInterval(getImage, 200);
+           }
   };
 };
