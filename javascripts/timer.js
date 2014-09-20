b92640e2966e5c935fb9ba6cad121a722e639667
fly1tkg <fly1tkg@gmail.com>
2014-09-21 05:54:58 +0900

    use moment.js

--- javascripts/timer.js
+++ javascripts/timer.js
@@ -2,19 +2,28 @@ function Timer(canvas) {
   var ctx = canvas.getContext('2d');
   ctx.scale(-1, 1);
 
+  var goal;
+
   function clear() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
   }
 
-  function draw() {
+  function draw(text) {
     ctx.font = "20pt Arial";
-    ctx.fillText("10:00", - canvas.width / 2, canvas.height / 2);
+    ctx.fillText(text, - canvas.width / 2, canvas.height / 2);
   }
 
   return {
+    start: function(sec) {
+             goal = moment().add(sec, 'second');
+           },
     getImage: function() {
                 clear();
-                draw();
+
+                var now = moment();
+                var rest = goal.subtract(now);
+
+                draw(rest.format('mm:ss'));
                 return canvas.toDataURL();
               }
   };
