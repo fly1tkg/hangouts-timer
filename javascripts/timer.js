1343595ef65b5328d7f6ff8a70098947970c23aa
fly1tkg <fly1tkg@gmail.com>
2014-09-21 06:59:18 +0900

    add minute and second button

--- javascripts/timer.js
+++ javascripts/timer.js
@@ -1,22 +1,23 @@
 function Timer(canvas) {
   var ctx = canvas.getContext('2d');
-  ctx.scale(-1, 1);
-
+  var duration = 0;
   var goal;
   var timerId;
+
+  ctx.scale(-1, 1);
+  draw('00:00');
 
   function clear() {
     ctx.clearRect(0, 0, - canvas.width, canvas.height);
   }
 
   function draw(text) {
+    clear();
     ctx.font = "20pt Arial";
     ctx.fillText(text, - canvas.width / 2, canvas.height / 2);
   }
 
   function getImage() {
-    clear();
-
     var now = moment();
     var rest = goal.unix() - now.unix();
 
@@ -31,9 +32,18 @@ function Timer(canvas) {
   }
 
   return {
-    start: function(sec, callback) {
-             goal = moment().add(sec, 'second');
+    start: function() {
+             goal = moment().add(duration, 'second');
+             duration = 0;
              timerId = setInterval(getImage, 200);
-           }
+           },
+    addMinutes: function(minutes) {
+                  duration += 60 * minutes;
+                  draw(moment(duration * 1000).utc().format('mm:ss'));
+                },
+    addSeconds: function(seconds) {
+                  duration += seconds;
+                  draw(moment(duration * 1000).utc().format('mm:ss'));
+                }
   };
 };
